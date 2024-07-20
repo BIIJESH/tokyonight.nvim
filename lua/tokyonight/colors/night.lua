5c59dc281d46b2c67ffe5646b6a9add75eb84964
BIIJESH <BIIJESH@proton.me>
2024-07-20 22:51:51 +0545

    edited night

--- lua/tokyonight/colors/night.lua
+++ lua/tokyonight/colors/night.lua
@@ -2,6 +2,7 @@ local ret = vim.deepcopy(require("tokyonight.colors.storm"))
 
 ---@type Palette
 return vim.tbl_deep_extend("force", ret, {
-  bg = "#1a1b26",
+
+  bg = "#020201",
   bg_dark = "#16161e",
 })
